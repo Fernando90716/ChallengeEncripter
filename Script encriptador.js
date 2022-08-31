5f927183ff260757dc704bc21f41fb409e57af4c
Fer <Hola@gmail>
2022-08-31 20:10:58 -0300

    Escribiendo JS, de momento con problemas

--- Script encriptador.js
+++ Script encriptador.js
@@ -1,9 +1,10 @@
-
-
-
+const entradaTexto = document.querySelector(".text-input") ;
+const textoResultado = document.querySelector(".texto-salida");
 
 function botonEncriptar(){
-
+    const textoEncriptado = encriptar(entradaTexto.value);
+    entradaTexto.value = "";
+    textoResultado.value = textoEncriptado;
 }
 
 function botonDesencriptar(){
@@ -11,5 +12,28 @@ function botonDesencriptar(){
 }
 
 function botonCopiar(){
+    copiar()
+}
 
+function encriptar(stringEntrada)
+    {
+        stringEntrada = stringEntrada.toLowerCase();
+        let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+
+        for (let index = 0; index < matrizCodigo.length; index++) 
+        {
+            if (stringEntrada.includes(matrizCodigo[index][0])) 
+            {
+                stringEntrada = stringEntrada.replaceAll(matrizCodigo[index][0], matrizCodigo[index][1]);
+            }
+        return stringEntrada;
+    }
+}
+
+function desencriptar(stringSalida){
+
+}
+
+function copiar() {
+    
 }
